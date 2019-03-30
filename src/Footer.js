7c4815ef5d3fc2c838bd5bdf584f1167d285defc
yogiesetiawan <46495276+yogiesetiawan@users.noreply.github.com>
2019-03-30 13:05:53 +0800

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+const Footer = (props) => {
+  const { textStyle, backFooter } = styles;
+  return (
+    <View style={backFooter}>
+      <Text style={textStyle}>Copyright @yogiesetiawan</Text>
+    </View>
+  );
+};
+const styles = {
+    backFooter: {
+      backgroundColor: '#e2682b',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 10,
+      paddingBottom: 10,
+      position: 'relative',
+    },
+    textStyle: {
+      fontSize: 18,
+      color: '#fff',
+      textAlign: 'center'
+    }
+}
+export default Footer;
---
+++ src/Weather.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+
+
+export default class Weather extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      forecast: {
+        main: '-',
+        description: '-',
+        temp: 0,
+        sunrise: 0,
+        sunset: 0,
+        pressure: 0,
+        humidity: 0,
+        sea_level: 0,
+        grnd_level: 0,
+        speed: 0,
+      }
+    };
+  }
+getWeather= () => {
+  let url = 'http://api.openweathermap.org/data/2.5/weather?q='+ this.state.city + '&appid=2e62f78ca2aa2fd0c2a9eef06bd7cea6&units=metric';
+  fetch(url)
+  .then((response) => response.json())
+  .then((responseJson) => {
+    console.log(responseJson);
+    this.setState({
+      forecast : {
+        main: responseJson.weather[0].main,
+        description: responseJson.weather[0].description,
+        temp: responseJson.main.temp,
+        sunrise: responseJson.sys.sunrise,
+        sunset: responseJson.sys.sunset,
+        pressure: responseJson.main.pressure,
+        humidity: responseJson.main.humidity,
+        sea_level: responseJson.main.sea_level,
+        grnd_level: responseJson.main.grnd_level,
+        speed: responseJson.wind.speed
+      }
+    });
+  }
+  );
+
+}
+  render() {
+    return (
+    <View style={styles.containerMain}>
+      <View style={styles.box2}>
+          <Text style={{ textAlign: 'center', paddingTop: 15,fontSize: 20, color: "white" }}> Masukan Nama Kota </Text>
+          <TextInput
+                style={{ height: 40, color: 'white'}}
+              placeholder=" Masukan Nama kota "
+              onChangeText={(city) => this.setState({ city })}
+            />
+            <Button
+              onPress={() => this.getWeather()}
+              title="Lihat"
+              color="black"
+              accessibilityLabel="Klik untuk melihat cuaca"
+            />
+      </View>
+
+      <View style={styles.box4}>
+        <View style={styles.button}>
+          <Text> Suhu: { this.state.forecast.temp} </Text>
+        </View>
+      </View>
+      <View style={styles.box4}>
+        <View style={styles.button}>
+          <Text> Cuaca: { this.state.forecast.main} </Text>
+        </View>
+        <View style={styles.button}>
+          <Text> Deskripsi: { this.state.forecast.description} </Text>
+        </View>
+      </View>
+
+</View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  containerMain: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    flexDirection: 'column'
+  },
+  box1: {
+    flex: 0.7,
+    backgroundColor: '#e2682b',
+  },
+  box2: {
+    flex: 0.4,
+    backgroundColor: '#e2682b',
+    marginLeft: 10,
+    marginRight: 10,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 10
+  },
+  box3: {
+    flex: 0.5,
+    backgroundColor: '#e2682b',
+    margin: 10,
+    marginRight: 10,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  box4: {
+    flex: 0.3,
+    backgroundColor: '#11d6c9',
+    //marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  box5: {
+    flex: 0.7,
+    backgroundColor: '#e2682b',
+    margin: 10
+  },
+  button: {
+    width: 180,
+    height: 40,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    flexDirection: 'row'
+  },
+  iconContainer: {
+    alignItems: 'center',
+    backgroundColor: '#feb401',
+    borderColor: '#feaf12',
+    //borderRadius: 15,
+    borderWidth: 1,
+    justifyContent: 'center',
+    height: 40,
+    width: 30,
+  },
+  icon: {
+    tintColor: '#fff',
+    height: 20,
+    width: 20,
+  }
+});
